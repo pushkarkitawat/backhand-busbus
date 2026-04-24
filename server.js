4068c1e9ee7108a54ad7ae03491f2de426d644aa
pushkarkitawat <pushkarkitawat123456@gmail.com>
2026-04-24 12:56:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/bb', (req, res) => {
   });
   
   // Start server
-  const PORT = process.env.DB_PORT || 5000;
+  const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
     console.log(`✅ Server running on http://localhost:${PORT}`);
   });
