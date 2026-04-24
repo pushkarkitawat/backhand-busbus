25a92fb5c21c9327c29c7684c11692ce31fc23c6
pushkarkitawat <pushkarkitawat123456@gmail.com>
2026-04-24 12:59:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ app.use('/api', login);
 app.get('/bb', (req, res) => {
     res.send('Transport Admin Backend Runningbusbus 🚚');
   });
-  
+  console.log("=== ENV DEBUG ===");
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
   // Start server
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
