2e7f17d6ad51b94aba1f732926de22610883d223
pushkarkitawat <pushkarkitawat123456@gmail.com>
2026-04-24 12:45:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-const express = require("express");
-const cors = require("cors");
-
+const express = require('express');
+const cors = require('cors');
 const app = express();
-app.use(cors());
+const dashboard = require('./routes/dashboard');
+const booking = require('./routes/booking');
+const login = require('./routes/login');
+require('dotenv').config();
+
 app.use(express.json());
-
-app.get("/back", (req, res) => {
-  res.send("Backend running ✅");
-});
-
-// ✅ FIX HERE
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+app.use(cors());
+app.use('/api/dashboard', dashboard);
+app.use('/api/booking', booking);
+app.use('/api', login);
+app.get('/', (req, res) => {
+    res.send('Transport Admin Backend Runningbusbus 🚚');
+  });
+  
+  // Start server
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`✅ Server running on http://localhost:${PORT}`);
+  });
