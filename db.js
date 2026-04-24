55d77b753059ca61c3dfafb94e85dbf359885c28
pushkarkitawat <pushkarkitawat123456@gmail.com>
2026-04-24 12:44:43 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,23 @@
-const mysql = require("mysql2/promise");
+const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port:process.env.PORT, // correct MySQL port
+const db = mysql.createPool({
+  host:process.env.DB_HOST,   // correct local IP or 127.0.0.1
+  user:process.env.DB_USER,
+  password:process.env.DB_PASSWORD,
+  database:process.env.DB_NAME,
+  port: process.env.DB_PORT,          // MUST be MySQL port, not 5000
+  waitForConnections: true,
+  connectionLimit: 10,
   ssl: {
+    // If TiDB Cloud gave you a CA file, use it:
+    ca: process.env.DB_SSL_CA
+      ? fs.readFileSync(path.join(__dirname, process.env.DB_SSL_CA))
+      : undefined,
     rejectUnauthorized: true
-  }
+  },
+  queueLimit: 0
 });
-console.log(process.env.DB_NAME);
-module.exports = pool;
+
+
+
+module.exports = db;
